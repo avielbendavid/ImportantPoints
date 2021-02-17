bcce60bf9682bd0c71b38823acbdc3d502882942
Aviel <avielbendavid22@gmail.com>
2021-02-17 19:36:55 +0200

    add task - threads synchronization

--- src/threads/task2/RequestHandler.java
+++ src/threads/task2/RequestHandler.java
@@ -6,26 +6,21 @@ public class RequestHandler implements Runnable {
 	private int requestId;
 
 	/**
-	 * 
 	 * this is a synchronized method
-	 *
 	 *
 	 * it is synchronized on the reference "this"
 	 *
+	 * it means that threads that want to enter this method (run it) must have the
+	 * key (only one) for "this "
 	 * 
-	 * it means that threads that want to enter this method (run it) 
-	 * must have the key (only one) for "this "
+	 * in JAVA every object has a key called - monitor(unique) 1 person - 1 monitor
+	 * 3 persons - 3 monitors every object has 1 monitor
 	 * 
-	 * in JAVA every object has a key called - monitor(unique)
-	 * 1 person - 1 monitor
-	 * 3 persons - 3 monitors
-	 * every object has 1 monitor
-	 *  
-	 * when we enter to the critical section of an object:
-	 *  we should present the key(monitor)
-	 *  
-	 *  *** the monitor has another name - lock flag ***
-	 *
+	 * when we enter to the critical section of an object: we should present the
+	 * key(monitor)
+	 * 
+	 * *** the monitor has another name - lock flag *** *** we can also synchronize
+	 * a block of code and not just a method ***
 	 */
 	@Override
 	public synchronized void run() {
