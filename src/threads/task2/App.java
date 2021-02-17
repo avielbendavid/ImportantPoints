729628527115976db90199218f58093c587427fc
Aviel <avielbendavid22@gmail.com>
2021-02-17 16:30:43 +0200

    add task of threads

---
+++ src/threads/task2/App.java
@@ -0,0 +1,16 @@
+package threads.task2;
+
+public class App {
+	public static void main(String[] args) {
+		RequestHandler requestHandler = new RequestHandler();
+
+		Thread ran = new Thread(requestHandler, "Ran");
+		Thread dan = new Thread(requestHandler, "Dan");
+		Thread aan = new Thread(requestHandler, "Aan");
+
+		ran.start();
+		dan.start();
+		aan.start();
+
+	}
+}
